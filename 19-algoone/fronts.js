51b705cb442ea02ab21f3f60a1d53c94ba39a355
RodYazdan <ibluetouch@gmail.com>
2022-04-17 21:06:49 -0400

    comit 2

--- 19-algoone/fronts.js
+++ 19-algoone/fronts.js
@@ -5,55 +5,66 @@ class Node {
 	}
 }
 
-// Add Front:Write a method that accepts a value and create a new node, assign it to the list head, and return a pointer to the new head node.
-
 class LinkedList {
 	constructor(value) {
 		this.head = {
 			value: value,
 			next: null
 		};
-		this.tail = this.head;
 		this.length = 1;
 	}
 
-
-    printList() {
-		let list = [];
-		let currentNode = this.head;
-		while (currentNode !== null) {
-			list.push(currentNode.value);
-			currentNode = currentNode.next;
-		}
-
-		console.log(list.join(' --> '));
-	}
+	// Add Front
+	// Write a method that accepts a value and create a new node, assign it to the list head, and return a pointer to the new head node.
 
     addFront(value) {
 		let newNode = new Node(value);
 		newNode.next = this.head;
 		this.head = newNode;
-
 		this.length++;
-		this.printList();
-		return this.length;
+		return this.head;
 	}
 
 
-    // Remove Front: Write a method to remove the head node and return the new list head node. If the list is empty, return null.
+	// Remove Front
+	// Write a method to remove the head node and return the new list head node. If the list is empty, return null.
     removeFront() {
-        this.head = this.head.next;
-		this.length--;
-		this.printList();
-		return this.length;
+		if(this.head){
+			this.head = this.head.next;
+			this.length--;
+			return this.head;
+		}
+		else{
+			return null;
+		}
+		
     }
-    // Front: Write a method to return the value (not the node) at the head of the list. If the list is empty, return null.
+
+	// Front
+	// Write a method to return the value (not the node) at the head of the list. If the list is empty, return null.
     front() {
-
+		if(this.head){
+			return this.head.data;
+		}
+		else{
+			return null;
+		}
     }
 
+	// Display
+	// Create display() that returns a string containing all list values. Build what you wish console.log(myList) did!
 
+	display() {
+		let runner=this.head
+		let myList=""
+		while(runner !== null){
+			myList+=runner.data
+			runner=runner.next
+		}
+		return myList
+		console.log(myList);
 
+    }
 
 }
 
